9cd7e7b42356fa450c5cec57c4cf826e6a69bc27
Travis Little <kingtrav@gmail.com>
2019-06-02 19:07:40 -0600

    mvp met on project 2

--- Project-II-Calculator/calculator/src/components/DisplayComponents/CalculatorDisplay.js
+++ Project-II-Calculator/calculator/src/components/DisplayComponents/CalculatorDisplay.js
@@ -1,2 +1,12 @@
 import React from 'react';
 import './Display.css';
+
+function CalculatorDisplay() {
+    return (
+            <div class="calculator-display">
+                <h2 class="calculator-text">0</h2>
+            </div>
+        );
+};
+
+export default CalculatorDisplay
